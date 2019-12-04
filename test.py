c42dd722d3cc9ccb3982f55347d51c3430344433
Anish Nag <anishnag00@gmail.com>
2019-12-03 16:50:54 -0800

    collate data

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from data import BoxScoreAdvancedV2
+from multiprocessing import Pool
 from nba_api.stats.static import players, teams
 from utils import save_obj, load_obj, parse_matchup
 
 import pandas as pd
+import os
 
-# game_data = {}
+game_data = {}
 team_history = load_obj('team_history')
 
 # find set of game_ids
@@ -14,32 +16,6 @@ for team, history in team_history.items():
     for _, game in history.iterrows():
         game_ids.add(game['GAME_ID'])
 
-# send request for advanced stats
-print('requesting advanced_stats...')
-advanced_stats = {}
-unfetched_game_ids = set()
-max_err_count = 5
-for i, game_id in enumerate(game_ids):
-    err_count = 0
-    while True:
-       try:
-           bs = BoxScoreAdvancedV2(game_id=game_id)
-           dfs = bs.get_data_frames()
-           advanced_stats[game_id] = dfs
-           print('%d/%d' % (i+1, len(game_ids)))
-           if i % 50 == 0:
-               save_obj(advanced_stats, 'advanced_stats')
-       except:
-           err_count += 1
-           if err_count == max_error_count:
-               unfetched_game_ids.add(game_id)
-               save_obj(unfetched_game_ids, 'unfetched_game_ids')
-               break
-       else:
-           break
-save_obj(advanced_stats, 'advanced_stats')
-
-'''
 # join by game_id
 print('joining by game_id...')
 for team, history in team_history.items():
@@ -51,97 +27,52 @@ for team, history in team_history.items():
 			game_data[game_id] = [game]
 		else:
 			game_data[game_id].append(game)
-print('number of games: %d' % len(game_data))
 
-print('collating data...')
-for i, game_id in enumerate(game_data):
-	print('game_id: %s [%d/%d]' % (game_id, i, len(game_data)))
-	data = game_data[game_id]
-	assert len(data) == 2
-	team1_data, team2_data = data[0], data[1]
-
-	stats = {}
-	matchup = team1_data['MATCHUP']
-	home_team, away_team = parse_matchup(matchup)
-	stats['home_team'] = home_team
-	stats['away_team'] = away_team
-	stats['game_date'] = team1_data['GAME_DATE']
-
-	lookup = {'W':'L', 'L':'W', None:None}
-	win_loss = team1_data['WL']
-	if home_team == team1_data['TEAM_ABBREVIATION']:
-		home_win = win_loss
-	else:
-		home_win = lookup[win_loss]
-	stats['home_win'] = home_win
-
-	team1_stats = team1_data.drop(['SEASON_ID', 'TEAM_ID', 'TEAM_ABBREVIATION', 'TEAM_NAME', 'GAME_ID', 'GAME_DATE', 'MATCHUP', 'WL'])
-	team2_stats = team2_data.drop(['SEASON_ID', 'TEAM_ID', 'TEAM_ABBREVIATION', 'TEAM_NAME', 'GAME_ID', 'GAME_DATE', 'MATCHUP', 'WL'])
-
-	bs = BoxScoreAdvancedV2(game_id=game_id)
-	dfs = bs.get_data_frames()
-	player_stats, team_stats = dfs[0], dfs[1]
-	stats['advanced_player_stats'] = player_stats
-	stats['advanced_team_stats'] = team_stats
-
-	if home_team == team1_data['TEAM_ABBREVIATION']:
-		stats['home_team_stats'] = team1_stats
-		stats['away_team_stats'] = team2_stats
-	else:
-		stats['home_team_stats'] = team2_stats
-		stats['away_team_stats'] = team1_stats
-
-	game_data[game_id] = stats
-
-save_obj(game_data, 'game_data')
-'''
-
-'''
+game_data = {}
 for team, history in team_history.items():
-	print('scrolling through history of team: %s' % team)
+	print('team: %s' % team)
 	for index, game in history.iterrows():
 		game_id = game['GAME_ID']
-		print('fetching advanced stats for game id: %s' % game_id)
+		print('joining game_id: %s [%d/%d]' % (game_id, len(game_data), len(game_ids)))
 
-		if game_id in game_data:
-			gd = game_data[game_id]
-			assert gd['home_team_stats'] is None or gd['away_team_stats'] is None
-			if gd['home_team_stats'] is not None:
-				key = 'away_team_stats'
-			else:
-				key = 'home_team_stats'
-			game_data[game_id][key] = game.drop(['SEASON_ID', 'TEAM_ID', 'TEAM_ABBREVIATION', 'TEAM_NAME', 'GAME_ID', 'GAME_DATE', 'MATCHUP', 'WL'])
-		else:
-			stats = {}
+		if not game_id in game_data:
+			data = {}
 			matchup = game['MATCHUP']
 			home_team, away_team = parse_matchup(matchup)
 
-			stats['home_team'] = home_team
-			stats['away_team'] = away_team
-			stats['game_date'] = game['GAME_DATE']
+			data['home_team'] = home_team
+			data['away_team'] = away_team
+			data['game_date'] = game['GAME_DATE']
 
-			lookup = {'W':'L', 'L':'W', None:None}
+			lookup = {'W':'L', 'L':'W', 'T':'T', None:None}
 			win_loss = game['WL']
 			if home_team == game['TEAM_ABBREVIATION']:
 				home_win = win_loss
 			home_win = lookup[win_loss]
-			stats['home_win'] = home_win
+			data['home_win'] = home_win
+			if home_win is None: continue
 
 			team_stats = game.drop(['SEASON_ID', 'TEAM_ID', 'TEAM_ABBREVIATION', 'TEAM_NAME', 'GAME_ID', 'GAME_DATE', 'MATCHUP', 'WL'])
-			bs = BoxScoreAdvancedV2(game_id=game_id)
-			dfs = bs.get_data_frames()
-			player_stats, team_stats = dfs[0], dfs[1]
+			dfs = load_obj(filename=game_id, dirname='advanced')
+			advanced_player_stats, advanced_team_stats = dfs[0], dfs[1]
 			if home_team == game['TEAM_ABBREVIATION']:
-				stats['home_team_stats'] = team_stats
-				stats['away_team_stats'] = None
+				data['home_team_stats'] = team_stats
+				data['away_team_stats'] = None
 			else:
-				stats['away_team_stats'] = team_stats
-				stats['home_team_stats'] = None
+				data['away_team_stats'] = team_stats
+				data['home_team_stats'] = None
 
-			stats['advanced_player_stats'] = player_stats
-			stats['advanced_team_stats'] = team_stats
+			data['advanced_player_stats'] = advanced_player_stats
+			data['advanced_team_stats'] = advanced_team_stats
 
-			game_data[game_id] = stats
+			game_data[game_id] = data
+		else:
+			if game_data[game_id]['home_team_stats'] is not None and game_data[game_id]['away_team_stats'] is not None:
+				continue
+			if game_data[game_id]['home_team_stats'] is None:
+				key = 'home_team_stats'
+			else:
+				key = 'away_team_stats'
+			game_data[game_id][key] = game.drop(['SEASON_ID', 'TEAM_ID', 'TEAM_ABBREVIATION', 'TEAM_NAME', 'GAME_ID', 'GAME_DATE', 'MATCHUP', 'WL'])
 
 save_obj(game_data, 'game_data')
-'''
